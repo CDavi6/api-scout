e5a3c65cbe5350f71808b51324b6f6749b5a0aaf
Clayton <claytondavison06@gmail.com>
2023-09-09 02:08:01 +1000

    idk

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Darkmodetoggle from "@/mycomponents/darkmodetoggle";
+
+export default function page() {
+  return (
+    <>
+      <Darkmodetoggle />
+    </>
+  );
+}
