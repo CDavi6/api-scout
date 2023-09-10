5685247286eec5e69a1e2e4eae4b8fa953e9c21d
Clayton <claytondavison06@gmail.com>
2023-09-10 21:26:08 +1000

    CATAAS page done. Will improve in the future, for now the basics is done.

--- app/api/cataas/page.tsx
+++ app/api/cataas/page.tsx
@@ -2,20 +2,28 @@
 
 import { FC } from "react";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Navbar from "@/mycomponents/Navbar";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface PageProps {}
 
 const Page: FC<PageProps> = ({}) => {
   const [cat, setCat] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getCatImage();
+  }, []);
 
   async function getCatImage() {
     setCat("");
-    const url = "https://cataas.com/c";
+    setIsLoading(true);
+
+    const url = "https://cataas.com/cat?width=512&height=512";
 
     const response = await fetch(url);
 
@@ -25,20 +33,33 @@ const Page: FC<PageProps> = ({}) => {
 
     console.log(response);
     setCat(response.url);
+    setIsLoading(false);
   }
 
   return (
     <>
       <Navbar />
-      <h1>Cat Image!</h1>
-      <Button
-        onClick={() => {
-          getCatImage();
-        }}
-      >
-        Get new cat!
-      </Button>
-      <img src={cat} alt="" />
+      <div className="flex flex-col justify-center items-center space-y-8">
+        <h1 className="font-bold text-2xl">Cat As A Service</h1>
+        <Button
+          onClick={() => {
+            getCatImage();
+          }}
+          className="w-32 hover:w-36 hover:h-14 transition-all active:w-32 active:h-10"
+        >
+          Get new cat!
+        </Button>
+        <div>
+          {isLoading && <Skeleton className="w-[512px] h-[512px] rounded-lg" />}
+          {!isLoading && <img src={cat} alt="" className="rounded-lg" />}
+        </div>
+        <h1 className="text-center">
+          Made with{" "}
+          <a className="hover:text-blue-600" href="https://cataas.com">
+            https://cataas.com
+          </a>
+        </h1>
+      </div>
     </>
   );
 };
